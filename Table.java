cf5ac83a5a3da94b9ce4ddb5f60524fe9cf00f3d
njim1572 <kochi.nakajima@gmail.com>
2017-12-01 13:05:17 -0700

    deal, countLeft added

--- Table.java
+++ Table.java
@@ -2,36 +2,41 @@ import java.util.*;
 
 public class Table{
 
-  ArrayList<Hai> tiles;
-  Stack<Hai> mountain; //mountain should be stack so we can just use a destructive .pop() to tsumo a Hai
-  Stack<Hai> wanpai;
+  public ArrayList<Hai> tiles;
+  public Stack<Hai> mountain; //mountain should be stack so we can just use a destructive .pop() to tsumo a Hai
+  public Stack<Hai> wanpai;
+  private Player[] players;
 
   public Table(){
     tiles = new ArrayList<Hai>();
-
   }
 
   /**
     *
     *@param tiles - all 136 tiles stored in here.
     */
-  public void init(ArrayList<Hai> tiles){
+  public void init(){
+    ArrayList<Hai> tiles = new ArrayList<Hai>();
     for(int j = 0; j < 4; j++){
       for(int i = 1; i < 10; i++){
         tiles.add(new Hai(i, "souzu"));//索子
         tiles.add(new Hai(i, "manzu"));//萬子
         tiles.add(new Hai(i, "pinzu"));//筒子
       }
-      for(int i = 0; i < 7; i++){
-        tiles.add(new Hai(0, "kaze"));//東
-        tiles.add(new Hai(1, "kaze"));//南
-        tiles.add(new Hai(2, "kaze"));//西
-        tiles.add(new Hai(3, "kaze"));//北
-        }
+
+      /** 0 - kaze = East
+        * 1 - kaze = South
+        * 2 - kaze = West
+        * 3 - kaze = North
+        * 0 - sangen = White
+        * 1 - sangen = Green
+        * 2 - sangen = Red
+        */
+      for(int i = 0; i < 4; i++){
+        tiles.add(new Hai(i, "kaze"));
+      }
       for(int i = 0; i < 3; i++){
-        tiles.add(new Hai(0, "sangen"));//白
-        tiles.add(new Hai(1, "sangen"));//發
-        tiles.add(new Hai(2, "sangen"));//中
+        tiles.add(new Hai(i, "sangen"));
       }
     }
 
@@ -39,11 +44,16 @@ public class Table{
 
     wanpai = this.makeWanpai(mountain);
 
+    int left = countLeft(mountain);
+    System.out.println(left);
+
+    deal();
+
   }
 
   public Stack<Hai> makeMountain(ArrayList<Hai> tiles){
     Stack<Hai> mountain = new Stack<Hai>();
-    Collections.shuffle(tiles);
+    //Collections.shuffle(tiles);
     for(int i = 0; i < tiles.size(); i++){
       mountain.push(tiles.get(i));
     }
@@ -58,14 +68,36 @@ public class Table{
     return wanpai;
   }
 
+  private void deal(Player[] players){
+    players = new Player[4];
+    for(int j = 0; j < 4; j++){
+      for(int i = 0; i < 13; i++){
+        players[j].tehai[i] = this.mountain.pop();
+      }
+    }
+    this.players = players;
+  }
 
-  // public static void main(String[] args){
-  //   ArrayList<Hai> tilesMain = new ArrayList<Hai>();
-  //   Stack<Hai> mountain = new Stack<Hai>();
-  //   Table t = new Table();
-  //   t.init(tilesMain);
-  //   for(int i = 0; i < 122; i++)
-  //     System.out.print(" " + t.mountain.pop() + " ");
-  // }
+  private int countLeft(Stack<Hai> mountain){
+    int counter = 0;
+    Stack<Hai> temp = new Stack<Hai>();
+    while(!mountain.empty()){
+      counter++;
+      temp.push(mountain.pop());
+    }
+    mountain = temp;
 
+    return counter;
+  }
+
+
+  public static void main(String[] args){ //To see if table is working.
+    ArrayList<Hai> tilesMain = new ArrayList<Hai>();
+    Stack<Hai> mountain = new Stack<Hai>();
+    Table t = new Table();
+    t.init();
+    while(!t.mountain.empty()){
+      System.out.print(" " + t.mountain.pop() + " ");
+    }
+  }
 }
